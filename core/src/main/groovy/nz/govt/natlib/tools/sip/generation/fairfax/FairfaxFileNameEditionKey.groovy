2ce41fc5242419ffb7a20604aaf43625b3263609
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2019-03-04 14:18:05 +1300

    FairfaxFileNameEditionKey add @ToString

--- core/src/main/groovy/nz/govt/natlib/tools/sip/generation/fairfax/FairfaxFileNameEditionKey.groovy
+++ core/src/main/groovy/nz/govt/natlib/tools/sip/generation/fairfax/FairfaxFileNameEditionKey.groovy
@@ -3,11 +3,13 @@ package nz.govt.natlib.tools.sip.generation.fairfax
 import groovy.transform.Canonical
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.Sortable
+import groovy.transform.ToString
 import groovy.util.logging.Slf4j
 
 @Canonical
 @EqualsAndHashCode
 @Sortable(includes = ['name', 'edition' ])
+@ToString
 @Slf4j
 class FairfaxFileNameEditionKey {
     String name
