e9aec505e1bd8575eb607682cd6eb65ae64106ec
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2019-06-28 15:10:52 +1200

    Correct tests to include Windows-specific root.
    ProcessorUtilsTest.groovy:
    - Correct tests to include Windows-specific root when it exists.

--- processor/src/test/groovy/nz/govt/natlib/tools/sip/generation/fairfax/processor/ProcessorUtilsTest.groovy
+++ processor/src/test/groovy/nz/govt/natlib/tools/sip/generation/fairfax/processor/ProcessorUtilsTest.groovy
@@ -1,6 +1,9 @@
 package nz.govt.natlib.tools.sip.generation.fairfax.processor
 
 import org.apache.commons.io.FilenameUtils
+import org.apache.commons.lang.SystemUtils
+
+import java.util.regex.Pattern
 
 import static org.hamcrest.core.Is.is
 import static org.junit.Assert.assertThat
@@ -27,10 +30,11 @@ class ProcessorUtilsTest {
         List<ProcessorOption> testOptions = [ ProcessorOption.ShowDirectoryOnly ]
 
         String expected = "filename.txt"
+        String osPrefix = getOsPrefix()
         // Can't really test ZERO_SEGMENT_FILE, since it will automatically have its parent because of how File is constructed.
         // For ZERO_SEGMENT_FILE_SLASH, it's treated as the root folder, which means it is a blank
-        assertThat("Full=${ZERO_SEGMENT_FILE_SLASH} with option=${testOptions} is=''",
-                ProcessorUtils.filePathAsSafeString(ZERO_SEGMENT_FILE_SLASH, testOptions), is(""))
+        assertThat("Full=${ZERO_SEGMENT_FILE_SLASH} with option=${testOptions} is='${osPrefix}'",
+                ProcessorUtils.filePathAsSafeString(ZERO_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}".toString()))
         assertThat("Full=${ONE_SEGMENT_FILE} with option=${testOptions} is=filename.txt",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions), is("filename.txt"))
         assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${expected}",
@@ -58,10 +62,11 @@ class ProcessorUtilsTest {
         List<ProcessorOption> testOptions = [ ProcessorOption.ShowDirectoryAndOneParent ]
 
         String expected = "parent1_filename.txt"
+        String osPrefix = getOsPrefix()
         assertTrue("Full=${ONE_SEGMENT_FILE} with option=${testOptions} ends with=filename.txt, actual=${ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions).endsWith("filename.txt"))
-        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_filename.txt",
-                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("_filename.txt"))
+        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_filename.txt",
+                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_filename.txt".toString()))
         assertThat("Full=${TWO_SEGMENT_FILE} with option=${testOptions} is=${expected}",
                 ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions), is(expected))
         assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=${expected}",
@@ -85,14 +90,15 @@ class ProcessorUtilsTest {
         List<ProcessorOption> testOptions = [ ProcessorOption.ShowDirectoryAndTwoParents ]
 
         String expected = "parent2_parent1_filename.txt"
+        String osPrefix = getOsPrefix()
         assertTrue("Full=${ONE_SEGMENT_FILE} with option=${testOptions} ends with=filename.txt, actual=${ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions).endsWith("filename.txt"))
-        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_filename.txt",
-                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("_filename.txt"))
+        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_filename.txt",
+                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_filename.txt".toString()))
         assertTrue("Full=${TWO_SEGMENT_FILE} with option=${testOptions} ends with=parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions).endsWith("parent1_filename.txt"))
-        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent1_filename.txt",
-                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("_parent1_filename.txt"))
+        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent1_filename.txt",
+                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent1_filename.txt".toString()))
         assertThat("Full=${THREE_SEGMENT_FILE} with option=${testOptions} is=${expected}",
                 ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions), is(expected))
         assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${expected}",
@@ -112,18 +118,19 @@ class ProcessorUtilsTest {
         List<ProcessorOption> testOptions = [ ProcessorOption.ShowDirectoryAndThreeParents ]
 
         String expected = "parent3_parent2_parent1_filename.txt"
+        String osPrefix = getOsPrefix()
         assertTrue("Full=${ONE_SEGMENT_FILE} with option=${testOptions} ends with=filename.txt, actual=${ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions).endsWith("filename.txt"))
-        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_filename.txt",
-                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("_filename.txt"))
+        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_filename.txt",
+                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_filename.txt".toString()))
         assertTrue("Full=${TWO_SEGMENT_FILE} with option=${testOptions} ends with=parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions).endsWith("parent1_filename.txt"))
-        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent1_filename.txt",
-                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("_parent1_filename.txt"))
+        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent1_filename.txt",
+                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent1_filename.txt".toString()))
         assertTrue("Full=${THREE_SEGMENT_FILE} with option=${testOptions} ends with=parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions).endsWith("parent2_parent1_filename.txt"))
-        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("_parent2_parent1_filename.txt"))
+        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent2_parent1_filename.txt".toString()))
         assertThat("Full=${FOUR_SEGMENT_FILE} with option=${testOptions} is=${expected}",
                 ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE, testOptions), is(expected))
         assertThat("Full=${FOUR_SEGMENT_FILE_SLASH} with option=${testOptions} is=${expected}",
@@ -138,51 +145,64 @@ class ProcessorUtilsTest {
     void verifyThatFilePathAsSafeStringWorksForFullPath() {
         List<ProcessorOption> testOptions = [ ProcessorOption.ShowFullPath ]
 
+        String osPrefix = getOsPrefix()
         assertTrue("Full=${ONE_SEGMENT_FILE} with option=${testOptions} ends with=filename.txt, actual=${ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions).endsWith("filename.txt"))
-        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_filename.txt",
-                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("_filename.txt"))
+        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_filename.txt",
+                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_filename.txt".toString()))
         assertTrue("Full=${TWO_SEGMENT_FILE} with option=${testOptions} ends with=parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions).endsWith("parent1_filename.txt"))
-        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent1_filename.txt",
-                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("_parent1_filename.txt"))
+        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is={osPrefix}_parent1_filename.txt",
+                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent1_filename.txt".toString()))
         assertTrue("Full=${THREE_SEGMENT_FILE} with option=${testOptions} ends with=parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions).endsWith("parent2_parent1_filename.txt"))
-        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("_parent2_parent1_filename.txt"))
+        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent2_parent1_filename.txt".toString()))
         assertTrue("Full=${FOUR_SEGMENT_FILE} with option=${testOptions} ends with=parent3_parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE, testOptions).endsWith("parent3_parent2_parent1_filename.txt"))
-        assertThat("Full=${FOUR_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent3_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE_SLASH, testOptions), is("_parent3_parent2_parent1_filename.txt"))
-        assertThat("Full=${FIVE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent4_parent3_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE_SLASH, testOptions), is("_parent4_parent3_parent2_parent1_filename.txt"))
+        assertThat("Full=${FOUR_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent3_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent3_parent2_parent1_filename.txt".toString()))
+        assertThat("Full=${FIVE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent4_parent3_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent4_parent3_parent2_parent1_filename.txt".toString()))
     }
 
     @Test
     void verifyThatFilePathAsSafeStringWorksForNoOption() {
         List<ProcessorOption> testOptions = [ ]
 
+        String osPrefix = getOsPrefix()
         assertTrue("Full=${ONE_SEGMENT_FILE} with option=${testOptions} ends with=filename.txt, actual=${ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE, testOptions).endsWith("filename.txt"))
-        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_filename.txt",
-                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("_filename.txt"))
+        assertThat("Full=${ONE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_filename.txt",
+                ProcessorUtils.filePathAsSafeString(ONE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_filename.txt".toString()))
         assertTrue("Full=${TWO_SEGMENT_FILE} with option=${testOptions} ends with=parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE, testOptions).endsWith("parent1_filename.txt"))
-        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent1_filename.txt",
-                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("_parent1_filename.txt"))
+        assertThat("Full=${TWO_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent1_filename.txt",
+                ProcessorUtils.filePathAsSafeString(TWO_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent1_filename.txt".toString()))
         assertTrue("Full=${THREE_SEGMENT_FILE} with option=${testOptions} ends with=parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE, testOptions).endsWith("parent2_parent1_filename.txt"))
-        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("_parent2_parent1_filename.txt"))
+        assertThat("Full=${THREE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(THREE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent2_parent1_filename.txt".toString()))
         assertTrue("Full=${FOUR_SEGMENT_FILE} with option=${testOptions} ends with=parent3_parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE, testOptions).endsWith("parent3_parent2_parent1_filename.txt"))
-        assertThat("Full=${FOUR_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent3_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE_SLASH, testOptions), is("_parent3_parent2_parent1_filename.txt"))
+        assertThat("Full=${FOUR_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent3_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(FOUR_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent3_parent2_parent1_filename.txt".toString()))
         assertTrue("Full=${FIVE_SEGMENT_FILE} with option=${testOptions} ends with=parent4_parent3_parent2_parent1_filename.txt, actual=${ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE, testOptions)}",
                 ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE, testOptions).endsWith("parent4_parent3_parent2_parent1_filename.txt"))
-        assertThat("Full=${FIVE_SEGMENT_FILE_SLASH} with option=${testOptions} is=_parent4_parent3_parent2_parent1_filename.txt}",
-                ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE_SLASH, testOptions), is("_parent4_parent3_parent2_parent1_filename.txt"))
+        assertThat("Full=${FIVE_SEGMENT_FILE_SLASH} with option=${testOptions} is=${osPrefix}_parent4_parent3_parent2_parent1_filename.txt}",
+                ProcessorUtils.filePathAsSafeString(FIVE_SEGMENT_FILE_SLASH, testOptions), is("${osPrefix}_parent4_parent3_parent2_parent1_filename.txt".toString()))
     }
 
+    static String getOsPrefix() {
+        String osPrefix
+        if (SystemUtils.IS_OS_WINDOWS) {
+            File testFile = new File("abc.txt")
+            List<String> splitPath = testFile.getCanonicalPath().split(Pattern.quote(File.separator))
+            osPrefix = splitPath.first().replace(":", "_")
+        } else {
+            osPrefix = ""
+        }
 
+        return osPrefix
+    }
 }
