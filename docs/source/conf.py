2e9b214365c3a02acc7d9cff64598919e0e1c09d
Charmaine Fajardo <charmaine.fajardo@dia.govt.nz>
2020-12-22 14:28:04 +1300

    Added 1.0.1-SNAPSHOT notes to documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'National Library of New Zealand'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0-SNAPSHOT'
+release = u'1.0.1-SNAPSHOT'
 
 
 # -- General configuration ---------------------------------------------------
